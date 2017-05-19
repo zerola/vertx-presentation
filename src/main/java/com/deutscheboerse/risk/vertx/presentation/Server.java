3e540be0d00b3dc46e4c67ce5e45c690053a66e5
Michal Zerola <michal.zerola@gmail.com>
2017-05-19 13:56:25 +0200

    Added HTML + JS

--- src/main/java/com/deutscheboerse/risk/vertx/presentation/Server.java
+++ src/main/java/com/deutscheboerse/risk/vertx/presentation/Server.java
@@ -2,14 +2,38 @@ package com.deutscheboerse.risk.vertx.presentation;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Server extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> future) {
+        EventBus eb = vertx.eventBus();
+        final AtomicLong timerID = new AtomicLong();
+        final AtomicLong messageID = new AtomicLong();
+        eb.consumer("start", message -> timerID.set(vertx.setPeriodic(1000, id -> {
+            String text = String.format("New message %s from Vert.x", messageID.incrementAndGet());
+            eb.publish("bcast", text);
+        })));
+        eb.consumer("stop", message -> vertx.cancelTimer(timerID.get()));
+
         Router router = Router.router(vertx);
+
+        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+        BridgeOptions options = new BridgeOptions()
+            .addInboundPermitted(new PermittedOptions().setAddress("start"))
+            .addInboundPermitted(new PermittedOptions().setAddress("stop"))
+            .addOutboundPermitted(new PermittedOptions().setAddress("bcast"));
+        sockJSHandler.bridge(options);
+
+        router.route("/eventbus/*").handler(sockJSHandler);
         router.route("/*").handler(StaticHandler.create("presentation"));
         vertx.createHttpServer()
                 .requestHandler(router::accept)
