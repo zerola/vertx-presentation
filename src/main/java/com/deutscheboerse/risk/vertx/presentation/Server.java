48afc1f9c551d5191051a1f642bae65ffac90d09
Michal Zerola <michal.zerola@gmail.com>
2017-05-20 22:22:45 +0200

    Fixing multiple timers

--- src/main/java/com/deutscheboerse/risk/vertx/presentation/Server.java
+++ src/main/java/com/deutscheboerse/risk/vertx/presentation/Server.java
@@ -9,6 +9,8 @@ import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Server extends AbstractVerticle {
@@ -16,13 +18,21 @@ public class Server extends AbstractVerticle {
     @Override
     public void start(Future<Void> future) {
         EventBus eb = vertx.eventBus();
-        final AtomicLong timerID = new AtomicLong();
+        final Collection<AtomicLong> timersID = new ArrayList<>();
         final AtomicLong messageID = new AtomicLong();
-        eb.consumer("start", message -> timerID.set(vertx.setPeriodic(1000, id -> {
-            String text = String.format("New message %s from Vert.x", messageID.incrementAndGet());
-            eb.publish("bcast", text);
-        })));
-        eb.consumer("stop", message -> vertx.cancelTimer(timerID.get()));
+        eb.consumer("start", message -> {
+            final AtomicLong newTimer = new AtomicLong();
+            newTimer.set(vertx.setPeriodic(1000, id -> {
+                String text = String.format("New message %s from Vert.x (timer: %d)", messageID.incrementAndGet(), id);
+                eb.publish("bcast", text);
+            }));
+            timersID.add(newTimer);
+        });
+        eb.consumer("stop", message -> {
+            timersID.forEach(id -> vertx.cancelTimer(id.get()));
+            timersID.clear();
+            messageID.set(0);
+        });
 
         Router router = Router.router(vertx);
 
